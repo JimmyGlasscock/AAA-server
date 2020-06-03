6e820c375e55ec60931e9d39f304217303c88a6c
jimmyglasscock <jamesglasscock99@gmail.com>
2020-06-03 12:27:54 -0600

    added getFriendRequests.php

---
+++ getFriendRequests.php
@@ -0,0 +1,34 @@
+<?php
+
+$servername = 'localhost';
+$username = 'app';
+$password = 'Andr0id43v3r';
+$dbname = 'AAA';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+	die("Connection Failed:" . $conn->connect_error);
+}
+
+$data = json_decode(file_get_contents('php://input'),true);
+$username = $data['username'];
+
+$query = "SELECT id FROM users WHERE  username='".$username."'";
+$result = $conn->query($query)->fetch_assoc();
+
+if(empty($result)){
+	die('username');
+}
+
+$userid = $result['id'];
+
+$friend_requests = array();
+
+$query = "SELECT users.id, username, firstname, lastname, accepted FROM users RIGHT JOIN friends ON users.id = friend_id WHERE (sender_id = '".$userid."' AND accepted='0') UNION SELECT users.id, username, firstname, lastname, accepted FROM users RIGHT JOIN friends ON users.id = friend_id WHERE (friend_id = '".$userid."' AND accepted='0');";
+$result = $conn->query($query);
+while($row = $result->fetch_assoc()){
+	array_push($friend_requests, $row);
+}
+
+die(json_encode($friend_requests));
+?>
